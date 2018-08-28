92906b7a43011a451efca23eb81c62309485227d
Daniel Gutierrez <daniel.gutierrez@degt.cl>
2018-08-28 20:47:44 -0300

    menu added

--- wp-content/themes/wp_boilerplate-master/header.php
+++ wp-content/themes/wp_boilerplate-master/header.php
@@ -38,7 +38,8 @@
          </form> 
           <a class="navbar-brand" href="#"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+            <i class="fas fa-bars text-white"></i>
+          <!-- <span class="navbar-toggler-icon"></span> -->
           </button>
           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div class="navbar-nav m-auto">
